a76ca176243886613d3898a096646a356838a68e
jesdone <jesdone@gmail.com>
2024-03-02 20:47:56 -0500

    task page

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { TextField } from "@radix-ui/themes";
 import React from "react";
 
 const NewTaskPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <TextField.Root>
+        <TextField.Input placeholder="Task" />
+      </TextField.Root>
+    </div>
+  );
 };
 
 export default NewTaskPage;
--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const TaskPage = () => {
   return (
     <div>
-      <Button>New Task</Button>
+      <Button>
+        <Link href="/tasks/new">New Task</Link>
+      </Button>
     </div>
   );
 };
